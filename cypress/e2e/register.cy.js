ebdf9cc1f523fda7f839ee8ce48b0c70408e67f7
NatalijaRadic <radic.natalija@gmail.com>
2023-03-19 19:31:14 +0100

    Homework one case positive and 5 negative

---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,68 @@
+/// <reference types="Cypress" />
+const {canvasToBlob} = require("blob-util");
+
+describe("Register page", () => {
+    it("Visit register page", () => {
+        cy.visit('https://gallery-app.vivifyideas.com/register')
+    });
+    it('Successful register', () => {
+        cy.visit("https://gallery-app.vivifyideas.com/register");
+        cy.get('input[id="first-name"]').type('Natalija');
+        cy.get('input[id="last-name"]').type('Radic');
+        cy.get('input[id="email"]').type('natka.radic@gmail.com');
+        cy.get('input[id="password"]').type('Naftalija1986');
+        cy.get('input[id="password-confirmation"]').type('Naftalija1986');
+        cy.get('input[type="checkbox"]').click();
+        cy.get('button[type="submit"]').click();
+    });
+    it('Unseccssful register case 1', () => {
+        cy.visit("https://gallery-app.vivifyideas.com/register");
+        cy.get('input[id="first-name"]').type('Natalija');
+        cy.get('input[id="last-name"]').type('Radic');
+        cy.get('input[id="email"]').type('');
+        cy.get('input[id="password"]').type('Naftalija1986');
+        cy.get('input[id="password-confirmation"]').type('Naftalija1986');
+        cy.get('input[type="checkbox"]').click();
+        cy.get('button[type="submit"]').click();
+    });
+    it('Unseccssful register case 2', () => {
+        cy.visit("https://gallery-app.vivifyideas.com/register");
+        cy.get('input[id="first-name"]').type('Natalija');
+        cy.get('input[id="last-name"]').type('Radic');
+        cy.get('input[id="email"]').type('natka.radic@gmail.com');
+        cy.get('input[id="password"]').type('');
+        cy.get('input[id="password-confirmation"]').type('');
+        cy.get('input[type="checkbox"]').click();
+        cy.get('button[type="submit"]').click();
+    });
+    it('Unseccssful register case 3', () => {
+        cy.visit("https://gallery-app.vivifyideas.com/register");
+        cy.get('input[id="first-name"]').type('');
+        cy.get('input[id="last-name"]').type('Radic');
+        cy.get('input[id="email"]').type('natka.radic@gmail.com');
+        cy.get('input[id="password"]').type('Naftalija1986');
+        cy.get('input[id="password-confirmation"]').type('Naftalija1986');
+        cy.get('input[type="checkbox"]').click();
+        cy.get('button[type="submit"]').click();
+    });
+    it('Unseccssful register case 4', () => {
+        cy.visit("https://gallery-app.vivifyideas.com/register");
+        cy.get('input[id="first-name"]').type('Natalija');
+        cy.get('input[id="last-name"]').type('');
+        cy.get('input[id="email"]').type('natka.radic@gmail.com');
+        cy.get('input[id="password"]').type('Naftalija1986');
+        cy.get('input[id="password-confirmation"]').type('Naftalija1986');
+        cy.get('input[type="checkbox"]').click();
+        cy.get('button[type="submit"]').click();
+    });
+    it('Unseccssful register case 5', () => {
+        cy.visit("https://gallery-app.vivifyideas.com/register");
+        cy.get('input[id="first-name"]').type('');
+        cy.get('input[id="last-name"]').type('Radic');
+        cy.get('input[id="email"]').type('natka.radic@gmail.com');
+        cy.get('input[id="password"]').type('Naftalija1986');
+        cy.get('input[id="password-confirmation"]').type('Naftalija1986');
+        cy.get('input[type="checkbox"]');
+        cy.get('button[type="submit"]').click();
+    });
+});
